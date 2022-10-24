17939de5a8a8d845fd74896a24f2c310a433a971
somkietacode <108131461+somkietacode@users.noreply.github.com>
2022-10-24 17:54:16 +0000

    Update __init__.py

--- Auto_Gradient_Boosting/__init__.py
+++ Auto_Gradient_Boosting/__init__.py
@@ -4,10 +4,7 @@ from .main import AGB
 if __name__ == "__main__" :
   x = np.matrix([[0,1],[1,4],[7,8],[50,23]])
   y = np.matrix([[2],[9],[23],[96]])
-  Lr = linearregression(x,y)
-  Beta, rss = Lr.leastsquare()
-  print(Beta)
-  print(rss)
+  agb = AGB(x,y,0.01)
   px = np.matrix([[4,7]])
-  r_y = Lr.predict(px)
+  r_y = agb.predict(px)
   print(r_y)
