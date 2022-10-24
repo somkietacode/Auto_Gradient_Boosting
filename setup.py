00a280e37d47271b731988ce8748d6e9bdb17970
somkietacode <108131461+somkietacode@users.noreply.github.com>
2022-10-24 17:57:59 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Auto_Gradient_Boosting',
+    version='1.0.0',
+    install_requires=[
+        'numpy',
+        'Awesome_Linear_Regression',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+)
